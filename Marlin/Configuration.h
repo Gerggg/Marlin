a5e04e3e487b80acd1c77cc75f3adb211c2f5a1c
Gerggg <greg.grsa@gmail.com>
2022-03-09 11:50:44 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Greg, TEVO Flash bugfix-2.0.x)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Greg, fTEVO Flash bugfix-2.0.x)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
